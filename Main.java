5d5c35760724d5b703acad044778133d8a7abb72
Alex Suglio <alexandersuglio@gmail.com>
2018-05-07 22:49:22 -0400

    the longest journey starts with a single step

---
+++ Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class Main{
+  public static void main(String [] args)
+  {
+    System.out.println("hey");
+    
+    int a; 
+    
+    Scanner txt = new Scanner(System.in);
+    
+    a = txt.nextInt();
+    
+    System.out.println("You typed in " + a);
+    
+    System.out.println("pretty nice");
+    System.out.println("type in a new number!");
+    
+    int b;
+    
+    b = txt.nextInt();
+    
+        System.out.println("you now entered " + b);
+
+    
+    
+    
+  }
+}
